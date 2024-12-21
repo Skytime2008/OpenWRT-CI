ced89f7da98cb367a2f454fbedc208959361f309
Skytime2008 <kylst@qq.com>
2024-12-21 22:55:33 +0800

    Update Handles.sh

--- Scripts/Handles.sh
+++ Scripts/Handles.sh
@@ -3,25 +3,25 @@
 PKG_PATCH="$GITHUB_WORKSPACE/wrt/package/"
 
 #预置HomeProxy数据
-if [ -d *"homeproxy"* ]; then
-	HP_RULES="surge"
-	HP_PATCH="homeproxy/root/etc/homeproxy"
-
-	chmod +x ./$HP_PATCH/scripts/*
-	rm -rf ./$HP_PATCH/resources/*
-
-	git clone -q --depth=1 --single-branch --branch "release" "https://github.com/Loyalsoldier/surge-rules.git" ./$HP_RULES/
-	cd ./$HP_RULES/ && RES_VER=$(git log -1 --pretty=format:'%s' | grep -o "[0-9]*")
-
-	echo $RES_VER | tee china_ip4.ver china_ip6.ver china_list.ver gfw_list.ver
-	awk -F, '/^IP-CIDR,/{print $2 > "china_ip4.txt"} /^IP-CIDR6,/{print $2 > "china_ip6.txt"}' cncidr.txt
-	sed 's/^\.//g' direct.txt > china_list.txt ; sed 's/^\.//g' gfw.txt > gfw_list.txt
-	mv -f ./{china_*,gfw_list}.{ver,txt} ../$HP_PATCH/resources/
-
-	cd .. && rm -rf ./$HP_RULES/
-
-	cd $PKG_PATCH && echo "homeproxy date has been updated!"
-fi
+#if [ -d *"homeproxy"* ]; then
+#	HP_RULES="surge"
+#	HP_PATCH="homeproxy/root/etc/homeproxy"
+#
+#	chmod +x ./$HP_PATCH/scripts/*
+#	rm -rf ./$HP_PATCH/resources/*
+#
+#	git clone -q --depth=1 --single-branch --branch "release" "https://github.com/Loyalsoldier/surge-rules.git" ./$HP_RULES/
+#	cd ./$HP_RULES/ && RES_VER=$(git log -1 --pretty=format:'%s' | grep -o "[0-9]*")
+#
+#	echo $RES_VER | tee china_ip4.ver china_ip6.ver china_list.ver gfw_list.ver
+#	awk -F, '/^IP-CIDR,/{print $2 > "china_ip4.txt"} /^IP-CIDR6,/{print $2 > "china_ip6.txt"}' cncidr.txt
+#	sed 's/^\.//g' direct.txt > china_list.txt ; sed 's/^\.//g' gfw.txt > gfw_list.txt
+#	mv -f ./{china_*,gfw_list}.{ver,txt} ../$HP_PATCH/resources/
+#
+#	cd .. && rm -rf ./$HP_RULES/
+#
+#	cd $PKG_PATCH && echo "homeproxy date has been updated!"
+#fi
 
 #移除Shadowsocks组件
 PW_FILE=$(find ./ -maxdepth 3 -type f -wholename "*/luci-app-passwall/Makefile")
@@ -43,9 +43,9 @@ if [ -f "$SP_FILE" ]; then
 fi
 
 #修复TailScale配置文件冲突
-TS_FILE=$(find ../feeds/packages/ -maxdepth 3 -type f -wholename "*/tailscale/Makefile")
-if [ -f "$TS_FILE" ]; then
-	sed -i '/\/files/d' $TS_FILE
-
-	cd $PKG_PATCH && echo "tailscale has been fixed!"
-fi
+#TS_FILE=$(find ../feeds/packages/ -maxdepth 3 -type f -wholename "*/tailscale/Makefile")
+#if [ -f "$TS_FILE" ]; then
+#	sed -i '/\/files/d' $TS_FILE
+#
+#	cd $PKG_PATCH && echo "tailscale has been fixed!"
+#fi
